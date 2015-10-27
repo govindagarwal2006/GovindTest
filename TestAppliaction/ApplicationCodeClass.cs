52632e4aa6372c44befcfb5a092c9bf0fb307929
govind <govind_agarwal@infosys.com>
2015-10-27 16:08:36 +0530

    govind at 4:10 pm

---
+++ TestAppliaction/ApplicationCodeClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppliaction
+{
+    public class ApplicationCodeClass
+    {
+        public string combineArrayStringWithSpace(string[] stringArray)
+        {
+            string str = default(string);
+
+            foreach (string item in stringArray)
+            {
+                str += item + " ";
+            }
+
+            return str.Trim();
+        }
+
+    }
+}
---
+++ UnitTestProj/UnitTestClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestAppliaction;
+
+namespace UnitTestProj
+{
+    [TestClass]
+    public class UnitTestClass
+    {
+        [TestMethod]
+        public void PossitiveSchenarioForChecking_combineArrayStringWithSpace_1()
+        {
+            string expectedResult = "This application is for testing purpose only";
+            string[] actualStringArray = new string[] { "This", "application", "is", "for", "testing", "purpose", "only" };
+            ApplicationCodeClass appObject = new ApplicationCodeClass();
+
+            string actualResult = appObject.combineArrayStringWithSpace(actualStringArray);
+            Assert.AreEqual<string>(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void PossitiveSchenarioForChecking_combineArrayStringWithSpace_2()
+        {
+            string expectedResult = "This appliaction is for failed case";
+            string[] actualStringArray = new string[] { "This", "application", "is", "for", "testing", "purpose", "only" };
+            ApplicationCodeClass appObject = new ApplicationCodeClass();
+
+            string actualResult = appObject.combineArrayStringWithSpace(actualStringArray);
+            Assert.AreEqual<string>(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void PossitiveSchenarioForChecking_combineArrayStringWithSpace_3()
+        {
+            string expectedResult = "This appliaction is for failed case";
+            string[] actualStringArray = new string[] { "This", "application", "is", "for", "testing", "purpose", "only" };
+            ApplicationCodeClass appObject = new ApplicationCodeClass();
+
+            string actualResult = appObject.combineArrayStringWithSpace(actualStringArray);
+            Assert.AreEqual<string>(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void PossitiveSchenarioForChecking_combineArrayStringWithSpace_4()
+        {
+            string expectedResult = "This appliaction is for failed case";
+            string[] actualStringArray = new string[] { "This", "application", "is", "for", "testing", "purpose", "only" };
+            ApplicationCodeClass appObject = new ApplicationCodeClass();
+
+            string actualResult = appObject.combineArrayStringWithSpace(actualStringArray);
+            Assert.AreEqual<string>(expectedResult, actualResult);
+        }
+    }
+}
